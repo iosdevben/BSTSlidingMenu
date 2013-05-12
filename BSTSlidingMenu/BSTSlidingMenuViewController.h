1dd19c9e100e163a1d17dc216b759e979241309e
Jesse Collis <jesse@jcmultimedia.com.au>
2013-05-12 12:32:39 +1000

    Reformatting header documentation to be more like appledoc

--- BSTSlidingMenu/BSTSlidingMenuViewController.h
+++ BSTSlidingMenu/BSTSlidingMenuViewController.h
@@ -5,10 +5,11 @@
 //
 
 
-// The direction that the menu will slide when it opens. If your
-// button is at the top of the screen, then you should set the opening
-// direction to down
-//
+/** The direction that the menu will slide when it opens. If your
+ *  button is at the top of the screen, then you should set the opening
+ *  direction to down.
+ *
+ */
 typedef NS_ENUM(NSInteger, BSTMenuOpeningDirection) {
   BSTMenuOpeningDirectionDown = 0,
   BSTMenuOpeningDirectionUp = 1,
@@ -16,11 +17,12 @@ typedef NS_ENUM(NSInteger, BSTMenuOpeningDirection) {
   BSTMenuOpeningDirectionRight = 3
 };
 
-// The alignment of menu to the button.
-// If your button is in the top-left corner of the screen, then
-// and you want the left side of the menu to be aligned with the
-// left side of the button, use BSTMenuPintLocationLeft
-//
+/** The alignment of menu to the button.
+ *  If your button is in the top-left corner of the screen,
+ *  and you want the left side of the menu to be aligned with the
+ *  left side of the button, use BSTMenuPintLocationLeft.
+ *
+ */
 typedef NS_ENUM(NSInteger, BSTMenuPinLocation) {
   BSTMenuPinLocationLeft = 0,
   BSTMenuPinLocationRight = 1,
@@ -34,81 +36,123 @@ static const CGFloat BSTMenuDefaultAnimationDelay = 0.f;
 
 @protocol BSTMenuDelegate
 
-// return the number of rows in the menu
+
+/** Returns the number of rows in the menu
+ *
+ *  @returns the number of rows in the menu.
+ */
 - (NSInteger)numberOfRowsInMenu;
 
-// return the size of the menu button
-// defaults to 44 x 44
+/** Return the size of the menu button
+ *  default value is 44 x 44
+ *  
+ *  @returns the size of the menu button
+ */
 - (CGSize)buttonSize;
 
-// return the width of the menu
-// defaults to 100
+/** Return the width of the menu
+ *  default value is 100
+ *
+ *  @returns the witdh of the menu
+ */
 - (NSInteger)menuWidth;
 
-// provide a view containing the row at the given index
-- (UIView *)viewForRowAtIndex:(NSInteger) index;
 
-// the height of the row at the given index
-- (CGFloat)heightForRowAtIndex:(NSInteger) index;
+/** Allows you to provide a view containing the row at a given index
+ *
+ *  @param an index
+ *  @returns a view containing the row at a given index
+ */
+- (UIView *)viewForRowAtIndex:(NSInteger)index;
+
+
+/** Returns the height of the row at a given index
+ *
+ *  @param an index
+ *  @returns the hiehgt of the row at a given index
+ */
+- (CGFloat)heightForRowAtIndex:(NSInteger)index;
 
 @optional
 
-// By default, this closes the menu if closeMenuWhenRowSelected is set to YES
-// and restarts the autoClose timer otherwise
-// Make sure you call this if you want to do other stuff when a menu item is selected
-- (void)didSelectRowAtIndex:(NSInteger) index;
+/** By default this closes the menu if closeMenuWhenRowSelected is set to YES
+ *  and restarts the autoClose timer otherwise.
+ *
+ *  Make sure you call this if you want to perform other actions when a
+ *  menu item is selected.
+ *
+ *  @param an index
+ */
+- (void)didSelectRowAtIndex:(NSInteger)index;
 
 @end
 
 @interface BSTSlidingMenuViewController : UIViewController<BSTMenuDelegate>
 
-// the delay to wait before animating the menu opening
-// defaults to BSTMenuDefaultAnimationDelay
+/** The delay to wait before animating the menu opening
+ *  default value is BSTMenuDefaultAnimationDelay
+ */
 @property (nonatomic, assign) CGFloat animationDelay;
 
-// the duration to animate the menu opening
-// defaults to BSTMenuDefaultAnimationDuration
+/** The duration to animate the menu opening
+ *  default value is BSTMenuDefaultAnimationDuration
+ */
 @property (nonatomic, assign) CGFloat animationDuration;
 
-// the colors for the menu button
-// default to white, gray, and light gray respectively
+/** the colours for the menu button
+ *  default to white, gray, and light gray respectively
+ */
 @property (nonatomic, strong) UIColor *buttonColor;
 @property (nonatomic, strong) UIColor *buttonActiveColor;
 @property (nonatomic, strong) UIColor *buttonHighlightColor;
 
-// the colors for the text
-// default to black
+/** The colours for the text
+ *  default value is black
+ */
 @property (nonatomic, strong) UIColor *buttonTextColor;
 @property (nonatomic, strong) UIColor *buttonHighlightTextColor;
 @property (nonatomic, strong) UIColor *buttonActiveTextColor;
 
-// Here you can modify the position of button title label
+/** Use this to modify the position of button title label
+ *
+ */
 @property (nonatomic) UIEdgeInsets buttonTitleInsets;
 
-// the title for the button
+/** The title for the button
+ *
+ */
 @property (nonatomic, strong) NSString *buttonTitle;
 
-// the font for the title
+/** The font for the title
+ *
+ */
 @property (nonatomic, strong) UIFont *buttonTitleFont;
 
-// automatically close the menu as soon as a menu item is selected
-// defaults to YES
+/** automatically close the menu as soon as a menu item is selected
+ *  default value is YES
+ */
 @property (nonatomic, assign) BOOL closeMenuWhenRowSelected;
 
-// the background color of the header, to hide the menu when it slides away
-// defaults to an ugly green so you don't forget to set this
+/** The background colour of the header, to hide the menu when it slides away
+ *  defaults to an ugly green
+ *
+ *  NOTE: Don't forget to set this!
+ */
 @property (nonatomic, strong) UIColor *headerBackgroundColor;
 
-// the direction to open the menu
-// defaults to down
+/** The direction to open the menu
+ *  default value is BSTMenuOpeningDirectionDown
+ */
 @property (nonatomic, assign) BSTMenuOpeningDirection openingDirection;
 
-// How the button and menu are aligned
-// defaults to BSTMenuPinLocationLeft
+/** How the button and menu are aligned
+ *  defaults to BSTMenuPinLocationLeft
+ */
 @property (nonatomic, assign) BSTMenuPinLocation pinLocation;
 
-// the menu will close after the period specified by timerLength if it's greater than 0
-// defaults to 0
+/** The menu will close after the period specified by timerLength if it's greater than 0
+ *  default value is 0
+ */
 @property (nonatomic, assign) NSTimeInterval timerLength;
 
 @end
